ae7149f59ad5710036daecc5f72d26d05984970b
Matt Travi <programmer@travi.org>
2019-05-04 21:54:57 -0500

    feat(rest-spread): enabled object rest-spread by default

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import env from '@babel/preset-env';
+import restSpread from '@babel/plugin-proposal-object-rest-spread';
 
 export default function () {
   return {
-    presets: [[env, {targets: {node: 'current'}}]]
+    presets: [[env, {targets: {node: 'current'}}]],
+    plugins: [[restSpread, {useBuiltIns: true}]]
   };
 }
--- test/unit/core-test.js
+++ test/unit/core-test.js
@@ -1,11 +1,13 @@
 import env from '@babel/preset-env';
+import restSpread from '@babel/plugin-proposal-object-rest-spread';
 import {assert} from 'chai';
 import buildConfig from '../../src';
 
 suite('core config', () => {
-  test('that preset-env targets the current version of node by default', () => {
+  test('that preset-env targets the current version of node by default and object rest-spread is enabled', () => {
     const config = buildConfig();
 
     assert.deepEqual(config.presets, [[env, {targets: {node: 'current'}}]]);
+    assert.deepEqual(config.plugins, [[restSpread, {useBuiltIns: true}]]);
   });
 });

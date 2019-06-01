4e05e96437206c4591b2d911f1c9d60e9f22e355
Matt Travi <programmer@travi.org>
2019-06-01 01:28:10 -0500

    feat(react): defined transformations for react components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import env from '@babel/preset-env';
 import restSpread from '@babel/plugin-proposal-object-rest-spread';
 
-export default function ({targets = {}, modules} = {}) {
+export default function ({targets = {}, modules, react} = {}) {
   return {
     presets: [
       [env, {
         targets: {node: targets.node || 'current'},
         ...(false === modules) && {modules: false},
         exclude: ['transform-regenerator', 'transform-async-to-generator']
-      }]
+      }],
+      ...react ? [require('@babel/preset-react')] : []
     ],
-    plugins: [[restSpread, {useBuiltIns: true}]]
+    plugins: [
+      [restSpread, {useBuiltIns: true}],
+      ...react ? [require('babel-plugin-inline-react-svg').default] : []
+    ]
   };
 }
---
+++ src/react-test.js
@@ -0,0 +1,11 @@
+import {assert} from 'chai';
+import buildConfig from '.';
+
+suite('react config', () => {
+  test('that react is configured when specified', () => {
+    const config = buildConfig({react: true});
+
+    assert.include(config.presets, require('@babel/preset-react'));
+    assert.include(config.plugins, require('babel-plugin-inline-react-svg').default);
+  });
+});
